327acddbc7a2d776ed8f021bed600146eeaf04cc
davidchoquerojas <choquerojasnedavid@gmail.com>
2017-10-09 22:06:36 -0500

    Cambio de control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductoComponent } from './producto/producto.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuLeftComponent } from './menu-left/menu-left.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductoComponent,
+    HeaderComponent,
+    MenuLeftComponent
   ],
   imports: [
     BrowserModule
